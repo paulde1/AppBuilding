1760188e31dc05e82d3aa3c4749f86b8c4ef2eef
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-19 20:22:53 -0400

    fresh app setup

--- nativeApp/App.js
+++ nativeApp/App.js
@@ -2,26 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}> BugMe </Text>
-      <Button title='Login or SignUp' />
-      <StatusBar style='auto' />
-    </View>
-  );
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    margin: 26,
-    borderColor: "green",
-    borderWidth: 5,
-    padding: 26,
-  },
-});
+const styles = StyleSheet.create({});
