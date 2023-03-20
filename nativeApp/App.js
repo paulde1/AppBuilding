fd7bcbac063c10d46eb6f1fa4bb4f734dfc2add6
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-19 21:41:06 -0400

    add function setup

--- nativeApp/App.js
+++ nativeApp/App.js
@@ -1,8 +1,60 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return <View></View>;
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const inputHandler = (enteredText) => {
+    setText(enteredText);
+  };
+
+  const addHandler = () => {
+    setTasks((tasks) => [...tasks, text]);
+  };
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.addItem}>
+        <TextInput
+          style={styles.inputText}
+          placeholder='I have to...'
+          onChangeText={inputHandler}
+        />
+        <Button title='Add' onPress={addHandler} />
+      </View>
+      <View style={styles.items}>
+        <Text> Things I need to do ... </Text>
+        {/* tasks?.map((task) => {<Text>{task}</Text>} */}
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    padding: 65,
+    paddingHorizontal: 16,
+  },
+  addItem: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomColor: "lightBlue",
+    borderBottomWidth: 1,
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: "#fdd",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+  items: {
+    flex: 5,
+  },
+});
